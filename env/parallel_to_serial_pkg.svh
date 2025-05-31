8d11d374c9a7e298501a1a837e0491e2bef02738
gujjuninjanoob <166725109+gujjuninjanoob@users.noreply.github.com>
2025-05-31 22:38:41 +0530

    Update parallel_to_serial_pkg.svh

--- env/parallel_to_serial_pkg.svh
+++ env/parallel_to_serial_pkg.svh
@@ -7,19 +7,17 @@
 `define PARALLEL_TO_SERIAL_PKG_SVH
  
 // Including other files.
-`include "parallel_to_serial_typedef_pkg.sv"
+`include "parallel_to_serial_params.sv"
 `include "parallel_to_serial_if.sv"
 
 package parallel_to_serial_pkg;
 
   // Import external packages 
   import uvm_pkg::*;
-  import base_pkg::*;
-  import parallel_to_serial_typedef_pkg::*;
+  import parallel_to_serial_params_pkg::*;
 
   // Include all supporting files for parallel_to_serial UVC
-  `include "base_macros.svh";
-  `include "parallel_to_serial_parameters.sv";
+  `include "uvm_macros.svh";
   `include "parallel_to_serial_config.sv";
   `include "parallel_to_serial_transaction.sv";
   `include "parallel_to_serial_driver.sv";
