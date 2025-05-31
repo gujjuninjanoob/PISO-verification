6d65c971e2d93c4d17ce11e6ce79673e55664ef3
gujjuninjanoob <166725109+gujjuninjanoob@users.noreply.github.com>
2025-05-31 23:07:00 +0530

    Update parallel_to_serial_config.svh

--- env/parallel_to_serial_config.svh
+++ env/parallel_to_serial_config.svh
@@ -16,26 +16,33 @@ class parallel_to_serial_config extends uvm_object;
   // Default : Enable -> 1
   // Disable -> 0
   bit en_checker_cov = 1'b1;
-  rand bit parity_en;
+  rand bit global_parity_en;
   rand bit [BAUD_RATE-1:0] baud_rate;
-  rand int num_pkts
+  rand int num_pkts;
 
     // Constraint for checker coverage
     constraint c_en_chk_cov {
     soft en_checker_cov == 1;}
     constraint parity_bit_valid{
-    parit_en inside {PARITY_DISABLED,PARITY_ENABLED};}
+    global_parit_en inside {PARITY_DISABLED,PARITY_ENABLED};}
+  //This we can use inside sequence for future purpose 
+  //  constraint dynamic_c {
+  //  if (cfg.global_parity_en==PARITY_ENABLED)
+  //      parity_enable == 1;
+  //  else
+  //      parity_enable == 0;}
+  
     constraint baud_rate_valid{
     baud_rate inside {BAUD_9600,BAUD_19200,BAUD_38400,BAUD_115200};}
     constraint num_packets_valid{
-    parit_en inside {[MIN_NUM_PACKETS:MAX_NUM_PACKETS]};}
+    num_pkts inside {[MIN_NUM_PACKETS:MAX_NUM_PACKETS]};}
 
   //--------------------------------------------------------------------
   // UVM factory registration
   //-------------------------------------------------------------------
   `uvm_object_utils_begin(parallel_to_serial_config)
     `uvm_field_int(en_checker_cov,UVM_ALL_ON)
-    `uvm_field_int(parity_en,UVM_ALL_ON)
+    `uvm_field_int(global_parity_en,UVM_ALL_ON)
     `uvm_field_int(baud_rate,UVM_ALL_ON)
     `uvm_field_int(num_pkts,UVM_ALL_ON) 
     
