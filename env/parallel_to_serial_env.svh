57b6cd357cb210f0e683fff572f09ddd2398b50f
gujjuninjanoob <166725109+gujjuninjanoob@users.noreply.github.com>
2025-06-01 22:47:42 +0530

    Update parallel_to_serial_env.svh

--- env/parallel_to_serial_env.svh
+++ env/parallel_to_serial_env.svh
@@ -6,9 +6,9 @@ class parallel_to_serial_env #(int WIDTH = 8) extends uvm_env;
   `uvm_component_param_utils(parallel_to_serial_env#(WIDTH))
 
   // Sub-components
-  parallel_to_serial_driver #(WIDTH)   driver;
-  parallel_to_serial_monitor #(WIDTH) monitor;
-  parallel_to_serial_scoreboard #(WIDTH) scoreboard;
+  parallel_to_serial_driver #(WIDTH)   m_driver;
+  parallel_to_serial_monitor #(WIDTH) m_monitor;
+  parallel_to_serial_scoreboard #(WIDTH) m_scoreboard;
 
   // Virtual interface
   virtual parallel_to_serial_if m_vif;
@@ -37,25 +37,25 @@ class parallel_to_serial_env #(int WIDTH = 8) extends uvm_env;
     end
 
     // Instantiate sub-components
-    driver = parallel_to_serial_driver#(WIDTH)::type_id::create("driver", this);
-    monitor = parallel_to_serial_monitor#(WIDTH)::type_id::create("monitor", this);
-    scoreboard = parallel_to_serial_scoreboard#(WIDTH)::type_id::create("scoreboard", this);
+    m_driver = parallel_to_serial_driver#(WIDTH)::type_id::create("m_driver", this);
+    m_monitor = parallel_to_serial_monitor#(WIDTH)::type_id::create("m_monitor", this);
+    m_scoreboard = parallel_to_serial_scoreboard#(WIDTH)::type_id::create("m_scoreboard", this);
   endfunction
 
   function void connect_phase(uvm_phase phase);
     super.connect_phase(phase);
 
-    // Connect driver and monitor to virtual interface
-    driver.m_vif = m_vif;
-    monitor.m_vif = m_vif;
+    // Connect m_driver and m_monitor to virtual interface
+    m_driver.m_vif = m_vif;
+    m_monitor.m_vif = m_vif;
 
     // Pass config object to subcomponents (optional, if they have m_cfg handles)
-    if (driver.has_field("m_cfg")) driver.m_cfg = m_cfg;
-    if (monitor.has_field("m_cfg")) monitor.m_cfg = m_cfg;
-    if (scoreboard.has_field("m_cfg")) scoreboard.m_cfg = m_cfg;
+    if (m_driver.has_field("m_cfg")) m_driver.m_cfg = m_cfg;
+    if (m_monitor.has_field("m_cfg")) m_monitor.m_cfg = m_cfg;
+    if (m_scoreboard.has_field("m_cfg")) m_scoreboard.m_cfg = m_cfg;
 
-    // Connect monitor analysis port to scoreboard
-    monitor.item_analysis_port.connect(scoreboard.item_export);
+    // Connect m_monitor analysis port to m_scoreboard
+    m_monitor.item_analysis_port.connect(m_scoreboard.item_export);
   endfunction
 
 endclass
